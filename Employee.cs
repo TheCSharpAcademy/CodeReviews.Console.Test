11ed11ddbdee9a751e0078926b834c2ee7b23d40
Pablo de Souza <pablod@teambaw.com.au>
2023-02-19 07:58:31 +1100

    first changes

---
+++ Employee.cs
@@ -0,0 +1,11 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+internal class Employee
+{
+    public void DoThing()
+    {
+        var unusedVariable = 5;
+
+        Console.WriteLine("Doing thing");
+    }
+}
